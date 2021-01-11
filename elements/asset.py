42658df1832a8686d121cff80b7ece6637178e53
Amirhossein <61879630+amirhossein-kz@users.noreply.github.com>
2021-01-11 19:42:55 +0330

    Update asset.py
    Former-commit-id: 706fe6942239533c7bd9e55a7a05d2c53dfaf496

--- elements/asset.py
+++ elements/asset.py
@@ -84,30 +84,33 @@ def apply_mask(image, seg_img, color = [244, 35, 232], alpha=0.5):
     # print(seg_img.shape)
     # print(seg_img.dtype)
     # np.save('img.npy', img)
-    np.save('seg.npy',seg_img)
+    try:
+        np.save('seg.npy',seg_img)
 
-    # img = np.load('img.npy')
-    seg_img = np.load('seg.npy')
-    mask = (seg_img == np.array([244, 35, 232]))[...,1].astype('uint8')
-    contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        # img = np.load('img.npy')
+        seg_img = np.load('seg.npy')
+        mask = (seg_img == np.array([244, 35, 232]))[...,1].astype('uint8')
+        contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    canvas = np.zeros_like(seg_img, np.uint8)
+        canvas = np.zeros_like(seg_img, np.uint8)
 
-    cnts = sorted(contours, key = cv2.contourArea, reverse=True)
-    n = len(cnts)
+        cnts = sorted(contours, key = cv2.contourArea, reverse=True)
+        n = len(cnts)
 
-    if n < 5:
-        for i in range(2):
-            cv2.drawContours(canvas, cnts, i,  (0,255,0), -1, cv2.LINE_AA)
-    else:
-        for i in range(2):
-            cv2.drawContours(canvas, cnts, i,  (0,255,0), -1, cv2.LINE_AA)
-        
-    mask_new = (canvas == np.array([0, 255, 0]))[...,1].astype('uint8')
-    mask_new = (cv2.resize(mask_new, (img.shape[1], img.shape[0])))
+        if n < 5:
+            for i in range(2):
+                cv2.drawContours(canvas, cnts, i,  (0,255,0), -1, cv2.LINE_AA)
+        else:
+            for i in range(2):
+                cv2.drawContours(canvas, cnts, i,  (0,255,0), -1, cv2.LINE_AA)
 
-    for c in range(3):
-        img[:,:,c]= np.where(mask_new == 1, img[:,:,c]*(1 - alpha)+alpha*color[c],img[:,:,c])
+        mask_new = (canvas == np.array([0, 255, 0]))[...,1].astype('uint8')
+        mask_new = (cv2.resize(mask_new, (img.shape[1], img.shape[0])))
+
+        for c in range(3):
+            img[:,:,c]= np.where(mask_new == 1, img[:,:,c]*(1 - alpha)+alpha*color[c],img[:,:,c])
+    except:
+        pass
     return img
 
 
