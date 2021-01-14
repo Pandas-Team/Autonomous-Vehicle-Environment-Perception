ae71d770a1e60463cda2c2d46940da601d1b47d9
abbasomidi77 <61683254+abbasomidi77@users.noreply.github.com>
2021-01-14 23:30:15 +0330

    Update asset.py
    Former-commit-id: 768dc1a77ccd9feaca392a7d8c4f25ec582fa5f9

--- elements/asset.py
+++ elements/asset.py
@@ -1,6 +1,25 @@
 import numpy as np
 import cv2
 
+def classic_distance(image, pts):
+	pts = pts.astype(dtype = "float32")
+	(tl, tr, br, bl) = pts
+	widthA = np.sqrt(((br[0] - bl[0]) * 2) + ((br[1] - bl[1]) * 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) * 2) + ((tr[1] - tl[1]) * 2))
+	maxWidth = max(int(widthA), int(widthB))
+	heightA = np.sqrt(((tr[0] - br[0]) * 2) + ((tr[1] - br[1]) * 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) * 2) + ((tl[1] - bl[1]) * 2))
+	maxHeight = max(int(heightA), int(heightB))
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	M = cv2.getPerspectiveTransform(pts, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	ppm=51.31
+	distance = (warped.shape[0]/ppm)
+	return distance
 
 def detect_lines(image):
     rho = 1  # precision in pixel, i.e. 1 pixel
